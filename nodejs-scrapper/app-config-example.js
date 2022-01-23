8c2f1fed92f76f0006732b8f74e0b8e248f6d777
Michal Škoula <skoula@outlook.com>
2022-01-23 02:46:54 +0100

    config example update

--- nodejs-scrapper/app-config-example.js
+++ nodejs-scrapper/app-config-example.js
@@ -5,6 +5,7 @@ config.pass = '*****';
 config.headless = true;
 config.slowMo = 0; // ms
 config.tecoUrl = "http://192.168.0.11";
+config.nodeMcuUrl = "http://192.168.0.172";
 config.browserUrl = "chromium";
 
 module.exports = config;
