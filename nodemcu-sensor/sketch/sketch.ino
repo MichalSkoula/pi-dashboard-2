7e4ff1907bb7ec210e07afbdcf0ded10b07f5c50
Michal Škoula <michal@skoula.cz>
2022-01-23 01:12:59 +0100

    nodemcu sketch update

--- nodemcu-sensor/sketch/sketch.ino
+++ nodemcu-sensor/sketch/sketch.ino
@@ -1,24 +1,99 @@
-#define VERSION "1.1"
- 
+// https://randomnerdtutorials.com/esp8266-web-server/
+// https://github.com/jlz3008/lm75
+
 #include <inttypes.h>
 #include <Wire.h>
 #include <lm75.h>
- 
+#include <ESP8266WiFi.h>
+#include <ArduinoJson.h> // by Benoit B. 
+
+// Replace with your network credentials
+const char* ssid     = "*****";
+const char* password = "*****";
+
+// Set web server port number to 80
+WiFiServer server(80);
+
+// Variable to store the HTTP request
+String header;
+
+// Current time
+unsigned long currentTime = millis();
+// Previous time
+unsigned long previousTime = 0; 
+// Define timeout time in milliseconds (example: 2000ms = 2s)
+const long timeoutTime = 2000;
+
+// LM75
 TempI2C_LM75 termo = TempI2C_LM75(0x48,TempI2C_LM75::nine_bits);
- 
+
 void setup()
 {
   Serial.begin(9600);
-  Serial.println("Start");
-  Serial.print("Actual temp ");
-  Serial.print(termo.getTemp());
-  Serial.println(" oC");
-  delay(2000);
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  // Print local IP address and start web server
+  Serial.println("");
+  Serial.println("WiFi connected.");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  server.begin();
 }
  
 void loop()
 {
-  Serial.print(termo.getTemp());
-  Serial.println(" oC");
-  delay(5000);
+  WiFiClient client = server.available();   // Listen for incoming clients
+
+  if (client) {                             // If a new client connects,
+    Serial.println("New Client.");          // print a message out in the serial port
+    String currentLine = "";                // make a String to hold incoming data from the client
+    currentTime = millis();
+    previousTime = currentTime;
+    while (client.connected() && currentTime - previousTime <= timeoutTime) { // loop while the client's connected
+      currentTime = millis();         
+      if (client.available()) {             // if there's bytes to read from the client,
+        char c = client.read();             // read a byte, then
+        Serial.write(c);                    // print it out the serial monitor
+        header += c;
+        if (c == '\n') {                    // if the byte is a newline character
+          // if the current line is blank, you got two newline characters in a row. that's the end of the client HTTP request, so send a response:
+          if (currentLine.length() == 0) {
+            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK) and a content-type so the client knows what's coming, then a blank line:
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type: application/json");
+            client.println("Connection: close");
+            client.println();
+
+            // generate JSON
+            const int capacity = JSON_OBJECT_SIZE(1);
+            StaticJsonDocument<capacity> result;
+            result["temp"] = termo.getTemp();
+
+            // write to client stream
+            serializeJson(result, client);
+  
+            // The HTTP response ends with another blank line
+            client.println();
+            // Break out of the while loop
+            break;
+          } else { // if you got a newline, then clear currentLine
+            currentLine = "";
+          }
+        } else if (c != '\r') {  // if you got anything else but a carriage return character,
+          currentLine += c;      // add it to the end of the currentLine
+        }
+      }
+    }
+    // Clear the header variable
+    header = "";
+    // Close the connection
+    client.stop();
+    Serial.println("Client disconnected.");
+    Serial.println("");
+  }
 }
